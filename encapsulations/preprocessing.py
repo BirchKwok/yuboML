b83fe4422a5b225698e46e3e647fe4f16860a614
birch <768907597@qq.com>
2020-02-07 20:59:47 +0800

    Some code has been updated

--- encapsulations/preprocessing.py
+++ encapsulations/preprocessing.py
@@ -1,26 +1,30 @@
 import numpy as np
 
+
 class StandardScaler:
+
     def __init__(self):
-        """不需要用户传入，但待用户查询时需要自动返回值的变量，后面需加上一个下划线_"""
         self.mean_ = None
         self.scale_ = None
 
-    def fit(self,X):
+    def fit(self, X):
         """根据训练数据集X获得数据的均值和方差"""
-        assert X.ndim == 2,"The dimension of X must be 2"
+        assert X.ndim == 2, "The dimension of X must be 2"
 
-        self.mean_ = np.array([np.mean(X[:,i] for i in range(X.shape[1]))])
+        self.mean_ = np.array([np.mean(X[:,i]) for i in range(X.shape[1])])
         self.scale_ = np.array([np.std(X[:,i]) for i in range(X.shape[1])])
+
         return self
 
-    def transform(self,X):
-        """将X根据这个StandarderScaler进行均值方差归一化处理"""
+    def transform(self, X):
+        """将X根据这个StandardScaler进行均值方差归一化处理"""
         assert X.ndim == 2, "The dimension of X must be 2"
-        assert self.mean_ is not None and self.scale_ is not None,"must fit before transform!"
-        assert X.shape[1] == len(self.mean_),"the feature number of X must be equal to mean_ and std_"
+        assert self.mean_ is not None and self.scale_ is not None, \
+               "must fit before transform!"
+        assert X.shape[1] == len(self.mean_), \
+               "the feature number of X must be equal to mean_ and std_"
 
-        resX = np.empty(shape=X.shape,dtype=float)
+        resX = np.empty(shape=X.shape, dtype=float)
         for col in range(X.shape[1]):
             resX[:,col] = (X[:,col] - self.mean_[col]) / self.scale_[col]
         return resX
